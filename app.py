74ced3e619be5821efca4e3bdb5bbc9ae3b0d7f9
Khyber Sen <kkysen@gmail.com>
2017-10-04 13:19:17 -0400

    login logout works

--- app.py
+++ app.py
@@ -17,12 +17,15 @@ from flask import Flask
 from flask import Response
 from flask import render_template
 from flask import request
+from flask import session
 from werkzeug.datastructures import ImmutableMultiDict
 
 # noinspection PyUnresolvedReferences
-import util.flask_utils
+from util.flask_utils import redirect_url
 
 app = Flask(__name__)
+
+users = {'Hello': 'World'}
 
 
 def br(n):
@@ -33,28 +36,40 @@ def br(n):
 @app.redirect_from('/')
 @app.route('/login')
 def login():
-    # type: () -> str
-
-    return render_template('login.jinja2')
+    # type: () -> Response
+    if 'username' in session:
+        return redirect_url(welcome)
+    else:
+        return render_template('login.jinja2')
 
 
 @app.route('/auth', methods=['post'])
 def authorize():
     # type: () -> Response
-    form = request.form  # type: ImmutableMultiDict
+    form = request.form
     username = form['username']
     password = form['password']
-    print(username, password)
-    return render_template(
-        'after_login.jinja2',
-        username=username,
-        password=password,
-        method=request.method,
-        br=br,
-    )
+    if username not in users:
+        return render_template('login.jinja2', failed='username')
+    if password != users[username]:
+        return render_template('login.jinja2', failed='password')
+    session['username'] = username
+    return redirect_url(welcome)
+
+
+@app.route('/welcome')
+def welcome():
+    username = session['username']
+    return render_template('welcome.jinja2', username=username, br=br)
+
+
+@app.route('/logout')
+def logout():
+    del session['username']
+    return redirect_url(login)
 
 
 if __name__ == '__main__':
     app.debug = True
-    app.secret_key = os.urandom(32);
+    app.secret_key = os.urandom(32)
     app.run()
