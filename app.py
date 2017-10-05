b6518522e19934a7e39309cce166f81033a1792a
Khyber Sen <kkysen@gmail.com>
2017-10-05 00:49:35 -0400

    fixed/added a few redirections and added documentation

--- app.py
+++ app.py
@@ -20,7 +20,6 @@ from flask import request
 from flask import session
 from werkzeug.datastructures import ImmutableMultiDict
 
-# noinspection PyUnresolvedReferences
 from util.flask_utils import redirect_url
 
 app = Flask(__name__)
@@ -30,6 +29,12 @@ users = {'Hello': 'World'}
 
 def br(n):
     # type: (int) -> str
+    """
+    Concisely create many <br> tags.
+
+    :param n: number of <br> to retur
+    :return: n <br> tags
+    """
     return '<br>' * n
 
 
@@ -37,7 +42,18 @@ def br(n):
 @app.route('/login')
 def login():
     # type: () -> Response
+    """
+    Load the login page or redirect to the welcome page if already logged in.
+
+    If the user hasn't logged in yet,
+    load the login page for them to log in,
+    but if the user has already logged in,
+    redirect straight to the welcome page.
+
+    :return: the login page or the welcome page redirection
+    """
     if 'username' in session:
+        print(session)
         return redirect_url(welcome)
     else:
         return render_template('login.jinja2')
@@ -46,7 +62,17 @@ def login():
 @app.route('/auth', methods=['post'])
 def authorize():
     # type: () -> Response
-    form = request.form
+    """
+    Authorize the user's attempted login and redirect them to the welcome page if successful.
+
+    If the user enters the correct login info,
+    add them to the session and redirect them to the welcome page,
+    but if either the username or password were wrong,
+    reload the same page with an error message on what they entered wrong.
+
+    :return: the same login page with an error message or the welcome page
+    """
+    form = request.form  # type: ImmutableMultiDict
     username = form['username']
     password = form['password']
     if username not in users:
@@ -59,12 +85,27 @@ def authorize():
 
 @app.route('/welcome')
 def welcome():
+    # type: () -> Response
+    """
+    Load the welcome page or redirect back to the login page if not logged in yet.
+
+    :return: the welcome page or the redirected login page
+    """
+    if 'username' not in session:
+        return redirect_url(login)
     username = session['username']
     return render_template('welcome.jinja2', username=username, br=br)
 
 
 @app.route('/logout')
 def logout():
+    # type: () -> Response
+    """
+    Logout the user by removing them from the session
+    and redirect them back to the original login page.
+
+    :return: the original login page
+    """
     del session['username']
     return redirect_url(login)
 
